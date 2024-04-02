e126e296055ecea1adde76dcc834f5e818c80bb5
TayDinhDev <taydinh2411@gmail.com>
2024-04-02 15:50:44 +0700

    ACCOMMODATION MANAGEMENT WEBSITE

---
+++ wwwroot/js/site.js
@@ -0,0 +1,18 @@
+﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your JavaScript code.
+<script>
+    // Khi tải trang, chọn mục menu đầu tiên là active
+    window.onload = function() {
+        const firstMenuItem = document.querySelector('.list-unstyled.component li:first-child a');
+    setActiveMenuItem(firstMenuItem);
+    }
+
+    function setActiveMenuItem(clickedItem) {
+        const menuItems = document.querySelectorAll('.list-unstyled.component li');
+        menuItems.forEach(item => item.classList.remove('active'));
+
+    clickedItem.parentNode.classList.add('active');
+    }
+</script>
